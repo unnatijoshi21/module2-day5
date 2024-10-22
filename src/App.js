387d8121dddc9d3a4d6db7cb6d70935dfcbbdea7
Unnati Joshi <unnatijoshi201@outlook.com>
2024-10-22 11:55:04 +0000

    this is day5 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Islandimg from "./Assets/image/img.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 classname="App">Unnati Joshi</h1>
+     {/* <img height="100px" width="100px" align="right" src="https://i2.wp.com/thenewcamera.com/wp-content/uploads/2016/01/Nikon-D500-Sample-Image-3.jpg"></img>
+      */} 
+      <img width="100px" height="100px" align="right" src={Islandimg} alt=""/>
+      <p> An undergraduate from CHRIST University, Bangalore with a degree of Bachlors of Business Administration Hons. (Finance); is currently an Intern at Deloitte. She come with a background in Finance, Business Analytics, Algorithms and Community Building.</p>
+      <br/>
+      <hr/>
+      <h3> Skills</h3>
+      <ul>
+        <li>PowerBI</li>
+        <li>SQL</li>
+        <li>Tableau</li>
+        <li>Java</li>
+        <li>Python</li>
+      <hr/>
+        
+      </ul>
     </div>
   );
 }
